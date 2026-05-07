a3686ed4d512712067364524d311d11dbfd92af8
Ciolac Sandu <ciolacsandu64@gmail.com>
2026-05-07 22:27:19 +0300

    Crearea WinForms

---
+++ F1Manager/Form1.cs
@@ -0,0 +1,14 @@
+using System.Windows.Forms;
+
+namespace F1Manager
+{
+    public class Form1 : Form
+    {
+        public Form1()
+        {
+            Text = "F1 Manager + Clasament";
+            Width = 800;
+            Height = 500;
+        }
+    }
+}
